b9401debb5999c1127500ee54d551d2a15171c5f
Shaun <scpour@gmail.com>
2024-04-05 13:39:23 -0400

    Mobile view almost done

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,18 @@
-console.log('HELLO');
+const form = document.querySelector('.card__input-form');
+const btn = document.getElementById('btn');
+const confirmBtn = document.getElementById('confirm-btn');
+const complete = document.querySelector('.completed');
 
-const test = () => {
-	console.log('this is a test');
-};
+btn.addEventListener('click', () => {
+	form.style.display = 'none';
+	complete.classList.add('completed-show');
+});
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+});
+
+confirmBtn.addEventListener('click', () => {
+	form.style.display = 'initial';
+	complete.classList.remove('completed-show');
+});
